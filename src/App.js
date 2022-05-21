d0e3f6f4c4d6bdb421721a0d198808e812b96f45
dmac <dmac100@users.noreply.github.com>
2022-05-21 14:35:55 +0100

    Added keybindings to change position

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ class App extends React.Component {
 			musicXml: musicXml,
 			playingNotes: [],
 			playing: false
-		}
+		};
 
 		this.onPositionChanged = this.onPositionChanged.bind(this);
 		this.createTracks = this.createTracks.bind(this);
@@ -70,6 +70,8 @@ class App extends React.Component {
 		this.onKeyClicked = this.onKeyClicked.bind(this);
 		this.togglePlay = this.togglePlay.bind(this);
 		this.advance = this.advance.bind(this);
+		this.onKeyDown = this.onKeyDown.bind(this);
+		this.setPositionSmooth = this.setPositionSmooth.bind(this);
 
 		this.playTimer = new PlayTimer();
 		this.playTimer.setAdvanceCallback(this.advance);
@@ -120,9 +122,103 @@ class App extends React.Component {
 		}
 	}
 
+	onKeyDown(event) {
+		if(!this.state.musicXml) return;
+
+		const PAGE_UP = 33;
+		const PAGE_DOWN = 34;
+		const HOME = 36;
+		const END = 35;
+		const ARROW_LEFT = 37;
+		const ARROW_UP = 38;
+		const ARROW_RIGHT = 39;
+		const ARROW_DOWN = 40;
+
+		// Move to previous or next note on arrow up and down.
+		if(event.keyCode == ARROW_UP || event.keyCode == ARROW_DOWN) {
+			let position = null;
+			for(let note of this.state.musicXml.notes) {
+				if(event.keyCode == ARROW_UP) {
+					if(note.startTime > this.state.position) {
+						if(!position || note.startTime < position) {
+							position = note.startTime;
+						}
+					}
+				} else if(event.keyCode == ARROW_DOWN) {
+					if(note.startTime < this.state.position) {
+						if(!position || note.startTime > position) {
+							position = note.startTime;
+						}
+					}
+				}
+			}
+
+			if(position) {
+				this.setPositionSmooth(position);
+			}
+		}
+
+		// Move to previous or next measure on arrow left and right.
+		if(event.keyCode == ARROW_LEFT || event.keyCode == ARROW_RIGHT) {
+			let position = null;
+			for(let measure of this.state.musicXml.measures) {
+				if(event.keyCode == ARROW_LEFT) {
+					if(measure.startTime < this.state.position) {
+						if(!position || measure.startTime > position) {
+							position = measure.startTime;
+						}
+					}
+				} else if(event.keyCode == ARROW_RIGHT) {
+					if(measure.startTime > this.state.position) {
+						if(!position || measure.startTime < position) {
+							position = measure.startTime;
+						}
+					}
+				}
+			}
+
+			if(position) {
+				this.setPositionSmooth(position);
+			}
+		}
+
+		// Move to beginning or end on home and end.
+		if(event.keyCode == HOME) {
+			this.setPositionSmooth(0);
+		} else if(event.keyCode == END) {
+			this.setPositionSmooth(musicXml.length);
+		}
+
+		// Jump back or forward on page up and down.
+		if(event.keyCode == PAGE_UP) {
+			this.setPositionSmooth(this.state.position - this.state.musicXml.length / 20);
+		} else if(event.keyCode == PAGE_DOWN) {
+			this.setPositionSmooth(this.state.position + this.state.musicXml.length / 20);
+		}
+	}
+
+	setPositionSmooth(newPosition) {
+		var position = this.state.position;
+
+		// Change position from current position to new position in steps.
+		var steps = 10;
+		var change = (newPosition - position) / steps;
+
+		function scroll() {
+			if(--steps >= 0) {
+				position += change;
+				this.setState({ position: Math.round(position) });
+			} else {
+				clearInterval(timer);
+			}
+		}
+
+		const timer = setInterval(scroll.bind(this), 5);
+	}
+
 	render() {
 		return (
-			<div className="App">
+			<div className="App" onKeyDown={this.onKeyDown} tabIndex="0">
 				<div className="topContent">
 					<Tracks tracks={this.state.tracks} onChange={this.onTrackChange}/>
 				</div>
@@ -131,7 +227,7 @@ class App extends React.Component {
 				<Scroll tracks={this.state.tracks} position={this.state.position} onScroll={this.onPositionChanged} musicXml={this.state.musicXml}/>
 
 				<div className="bottomContent">
-					<Keyboard playingNotes={this.state.playingNotes} onClick={this.onKeyClicked}/>
+					<Keyboard playingNotes={this.state.playingNotes} onClick={this.onKeyClicked} onKeyUp={this.onKeyUp}/>
 					<Controls onPlay={this.togglePlay} playing={this.state.playing}/>
 				</div>
 			</div>
